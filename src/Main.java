6ad6aac1b49afdb54d55374cbb93bfc1a7b295fb
IsraelTello <itello@rapi.tech>
2026-04-28 23:57:27 -0500

    MOD Main.java para que pida los datos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema de registro de pedidos");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== SISTEMA DE REGISTRO DE PEDIDOS ===");
+
+        System.out.print("Ingrese ID del pedido: ");
+        String idPedido = scanner.nextLine();
+
+        System.out.print("Ingrese nombre del cliente: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Ingrese DNI del cliente: ");
+        String dni = scanner.nextLine();
+
+        System.out.print("Ingrese monto del pedido: ");
+        double monto = scanner.nextDouble();
+
+        Cliente cliente = new Cliente(nombre, dni);
+
+        Pedido pedido = new Pedido(idPedido, cliente, monto);
+
+        LegacyBillingSystem legacyBillingSystem = new LegacyBillingSystem();
+
+        FacturaService facturaService = new BillingAdapter(legacyBillingSystem);
+
+        RegistrarPedidoService registrarPedidoService =
+                new RegistrarPedidoService(facturaService);
+
+        registrarPedidoService.registrarPedido(pedido);
+
+        scanner.close();
     }
 }
